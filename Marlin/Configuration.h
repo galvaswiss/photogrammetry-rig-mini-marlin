f66f2d3f1f5139f246b8dbc749194e4f82dd485e
alexdietsche <alex.dietsche@hotmail.com>
2025-07-08 15:06:21 +0200

    remove lengthy extrude prevention

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -857,7 +857,7 @@
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
-#define PREVENT_LENGTHY_EXTRUDE
+// #define PREVENT_LENGTHY_EXTRUDE
 #define EXTRUDE_MAXLENGTH 200
 
 //===========================================================================
